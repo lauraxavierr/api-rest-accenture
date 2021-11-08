47c77456f84b7919e6a9673073c087c60f5678a8
lauraxavierr <laurausual@gmail.com>
2021-11-08 18:18:45 -0300

    fix: atualizando estrutura do sequelize;

--- src/database/migrations/20211107143205-users_accen.js
+++ src/database/migrations/20211107143205-users_accen.js
@@ -1,56 +1,56 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
- return queryInterface.createTable('users', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      nome: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: { 
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      senha: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      telefone: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      token: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      ultimo_login: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      expira_login: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    })
-  },
+	up: async (queryInterface, Sequelize) => {
+		return queryInterface.createTable('users', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			nome: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			email: { 
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true,
+			},
+			senha: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			telefone: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			token: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			ultimo_login: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			expira_login: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		});
+	},
 
 
-  down: async queryInterface => {
-    return queryInterface.dropTable('users');
-  }
+	down: async queryInterface => {
+		return queryInterface.dropTable('users');
+	}
 };
